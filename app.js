ed2760d580170939e3dfa0b91c560fc0251c82eb
DenisB44 <denis.boisteux@gmail.com>
2016-03-03 22:19:08 +0100

    Update app.js
    Version stable de la connexion avec une seule page pour accueil et vote

--- app.js
+++ app.js
@@ -1,130 +1,153 @@
 var app = angular.module('nideputenisoumises', []);
 
-/*
-app.controller('LoginCtrl', ['$scope', function ($scope) {
-  
-}]);
-
-app.controller('MainCtrl', ['$scope', function ($scope) {
-
-}]);
-
-app.controller('VoteCtrl', ['$scope', function ($scope) {
-  
-}]);
-*/
-
 app.directive('headTemplate', function(){
-    return{
-        restrict: 'C',
-        templateUrl: 'head.html'
-    };
+	return{
+		restrict: 'C',
+		templateUrl: 'head.html'
+	};
 });
 
 app.directive('navTemplate', function(){
-    return {
-      
-        restrict: 'C',
-        templateUrl: 'nav.html'
-    };
-    
+	return {
+		restrict: 'C',
+		templateUrl: 'nav.html'
+	};
+
 });
 
 app.directive('footerTemplate', function(){
-    return {
-        restrict: 'C',
-        templateUrl: 'footer.html'
-    };
+	return {
+		restrict: 'C',
+		templateUrl: 'footer.html'
+	};
 });
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
-	
-  $scope.UserName = "Pas connecté";
-  $scope.connecte = false;
-  $scope.affichageFin = true;
-  $scope.compteur = 0;  
-  $scope.questions = [
-    {id:"505", ordre:"1",vote: "",question:"Whats your (full) name?"},
-    {id:"402", ordre:"2",vote: "",question:"How old are you?"},
-    {id:"302", ordre:"3",vote: "",question:"Whats your Birthday?"},
-    {id:"111", ordre:"4",vote: "",question:"What starsign does that make it?"},
-    {id:"222", ordre:"5",vote: "",question:"Whats your favourite colour?"},
-    {id:"333", ordre:"6",vote: "",question:"Whats your lucky number?"},
-    {id:"444", ordre:"7",vote: "",question:"Do you have any pets?"},
-    {id:"555", ordre:"8",vote: "",question:"Where are you from?"},
-    {id:"666", ordre:"9",vote: "",question:"What starsign does that make it?"},
-    {id:"999", ordre:"10",vote: "",question:"How tall are you?"}
-  ];
-  
-  $scope.fctDeco= function(){
+
+	$scope.UserName = "Visiteur";
+	$scope.connecte = false;
+	$scope.accueil = true;
+	$scope.affichageVote = true;
+	$scope.affichageFin = true;
+	$scope.partieVote = true;
+
+	$scope.compteur = 0;  
+	$scope.questions = [
+	                    {id:"505", ordre:"1",vote: "",question:"Whats your (full) name?"},
+	                    {id:"402", ordre:"2",vote: "",question:"How old are you?"},
+	                    {id:"302", ordre:"3",vote: "",question:"Whats your Birthday?"},
+	                    {id:"111", ordre:"4",vote: "",question:"What starsign does that make it?"},
+	                    {id:"222", ordre:"5",vote: "",question:"Whats your favourite colour?"},
+	                    {id:"333", ordre:"6",vote: "",question:"Whats your lucky number?"},
+	                    {id:"444", ordre:"7",vote: "",question:"Do you have any pets?"},
+	                    {id:"555", ordre:"8",vote: "",question:"Where are you from?"},
+	                    {id:"666", ordre:"9",vote: "",question:"What starsign does that make it?"},
+	                    {id:"999", ordre:"10",vote: "",question:"How tall are you?"}
+	                    ];
+
+	$scope.fctDeco= function(){
+		console.log("Methode : fctDeco");
 		var divToken = document.getElementById('varToken');
 		varaccess_token = divToken.innerHTML;
 		//alert(varaccess_token);
 		$scope.disconnectUser(varaccess_token);
 	};
-   
-	$scope.disconnectUser = function (access_token) {
-		//alert(varaccess_token);
-		  var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' + access_token;
 
-		  // Exécuter une requête GET asynchrone.
-		  $.ajax({
+	$scope.disconnectUser = function (access_token) {
+		console.log("Methode : disconnectUser");
+		//alert(varaccess_token);
+		var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' + access_token;
+
+		// Exécuter une requête GET asynchrone.
+		$.ajax({
 			type: 'GET',
 			url: revokeUrl,
 			async: false,
 			contentType: "application/json",
 			dataType: 'jsonp',
 			success: function(nullResponse) {
-			  // Effectuer une action maintenant que l'utilisateur est dissocié
-			  // La réponse est toujours non définie.
+				// Effectuer une action maintenant que l'utilisateur est dissocié
+				// La réponse est toujours non définie.
 			},
 			error: function(e) {
-			  // Gérer l'erreur
-			  // console.log(e);
-			  // Orienter éventuellement les utilisateurs vers une dissociation manuelle en cas d'échec
-			  // https://plus.google.com/apps
+				// Gérer l'erreur
+				// console.log(e);
+				// Orienter éventuellement les utilisateurs vers une dissociation manuelle en cas d'échec
+				// https://plus.google.com/apps
 			}
-		  });
-		};
+		});
+	};
 
-  $scope.majConnexion = function (name){	 
-	 $scope.connecte = !$scope.connecte;
-	 $scope.UserName = name;
-  }
-  
-  $scope.oui = function (){
-   $scope.questions[$scope.compteur].vote = "ok";
-   $scope.compteur++;
-   if($scope.compteur == 10){
-     $scope.fin();
-   }
-  }
-   
-  $scope.non = function (){
-    $scope.questions[$scope.compteur].vote = "ko";
-    $scope.compteur++;
-    if($scope.compteur == 10){
-      $scope.fin();
-    }
-  }
-  
-  $scope.nspp = function (){
-    $scope.questions[$scope.compteur].vote = "nspp";
-    $scope.compteur++;
-    if($scope.compteur == 10){
-      $scope.fin();
-    }
-  }
-   
-  $scope.fin = function(){
-    console.log($scope.questions);
-    $scope.affichageVote = true;
-    $scope.affichageFin = false;
-  }
-  
-  
+	$scope.majConnexion = function (name){	 
+		console.log("Methode : majConnexion");
+		$scope.connecte = !$scope.connecte;
+		if($scope.connecte){
+			$scope.accueil = false;
+		}
+		else {
+			$scope.accueil = "Visiteur";
+			$scope.accueil = true;
+			$scope.affichageVote = true;
+			$scope.affichageFin = true;
+			$scope.partieVote = true;			
+		}
+		$scope.UserName = name;
+	}
+
+	$scope.home = function (){	 
+		console.log("Methode : accueil");
+		if($scope.connecte){
+			$scope.accueil = false;
+			$scope.affichageVote = true;
+			$scope.affichageFin = true;
+			$scope.partieVote = true;
+		}
+	}
+
+	$scope.voter = function (){	 
+		console.log("Methode : voter");
+		if($scope.connecte){
+			$scope.accueil = true;
+			$scope.partieVote = false;
+			$scope.affichageVote = false;
+		}
+	}
+
+	$scope.oui = function (){
+		console.log("Methode : oui");
+		$scope.questions[$scope.compteur].vote = "ok";
+		$scope.compteur++;
+		if($scope.compteur == 10){
+			$scope.fin();
+		}
+	}
+
+	$scope.non = function (){
+		console.log("Methode : non");
+		$scope.questions[$scope.compteur].vote = "ko";
+		$scope.compteur++;
+		if($scope.compteur == 10){
+			$scope.fin();
+		}
+	}
+
+	$scope.nspp = function (){
+		console.log("Methode : nspp");
+		$scope.questions[$scope.compteur].vote = "nspp";
+		$scope.compteur++;
+		if($scope.compteur == 10){
+			$scope.fin();
+		}
+	}
+
+	$scope.fin = function(){
+		console.log("Methode : fin");
+		$scope.affichageVote = true;
+		$scope.affichageFin = false;
+	}
+
+
 }]);
 
 
-    
-    
+
